5f1a97c0db04e1da7069987e46547f674858fb12
Eliya Mazor <eliyahmazor@gmail.com>
2024-03-20 15:08:57 +0200

    Add dragging option to the text

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,12 +1,18 @@
 'use script'
 
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
+
 let gElCanvas
 let gCtx
+let gMousePos
 
 function onInit() {
   gElCanvas = document.querySelector('canvas')
   gCtx = gElCanvas.getContext('2d')
   gMeme = getMeme(CURR_MEME_KEY)
+
+  addListeners()
+  // resizeCanvas()
 
   renderGallery()
 
@@ -14,7 +20,8 @@ function onInit() {
 }
 
 function renderFromStorage() {
-  document.querySelector('.meme-text').value = gMeme.lines[gMeme.selectedLineIdx].txt
+  document.querySelector('.meme-text').value =
+    gMeme.lines[gMeme.selectedLineIdx].txt
   const img = new Image()
   const meme = findImgById(gMeme.selectedImgId)
 
@@ -67,18 +74,17 @@ function onDrawText(txt) {
 
 function drawText() {
   // gCtx.beginPath()
-  gCtx.lineWidth = 5
-  gCtx.strokeStyle = 'orange'
-  gCtx.fillStyle = 'white'
+  gMeme.lines.forEach((line) => {
+    gCtx.lineWidth = 5
+    gCtx.fillStyle = 'white'
 
-  gCtx.font = '45px Arial'
-  gCtx.fillText(gMeme.lines.forEach(line => {
-    
+    gCtx.font = '45px Arial'
+    gCtx.strokeStyle = 'orange'
+    line.fontSize = 45
+    line.isDrag = false
+
+    gCtx.fillText(line.txt, line.x, line.y)
   })
-    gMeme.lines[gMeme.selectedLineIdx].txt,
-    gMeme.lines[gMeme.selectedLineIdx].x,
-    gMeme.lines[gMeme.selectedLineIdx].y
-  )
   // gCtx.closePath()
 }
 
@@ -94,4 +100,120 @@ function onSave() {
 function onClearCanvas() {
   gMeme = {}
   gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function addListeners() {
+  addMouseListeners()
+  addTouchListeners()
+
+  window.addEventListener('resize', () => {
+    resizeCanvas()
+
+    renderCanvas()
+  })
+}
+
+function addMouseListeners() {
+  gElCanvas.addEventListener('mousedown', onDown)
+  gElCanvas.addEventListener('mousemove', onMove)
+  gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+  gElCanvas.addEventListener('touchstart', onDown)
+  gElCanvas.addEventListener('touchmove', onMove)
+  gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+  gMousePos = getEvPos(ev)
+
+  const line = txtClicked()
+  if(!line) return
+
+  setTextDrag(line)
+  document.body.style.cursor = 'grabbing'
+}
+
+function onMove(ev) {
+  const line = gMeme.lines.find((line) => {
+    var { isDrag } = line
+    return isDrag
+  })
+  if (!line) return
+
+  const pos = getEvPos(ev)
+
+  const dx = pos.x - gMousePos.x
+  const dy = pos.y - gMousePos.y
+
+  moveLine(line, dx, dy)
+
+  gMousePos = pos
+
+  renderMeme()
+  drawText()
+}
+
+function onUp() {
+  const line = txtClicked()
+  if(!line) return
+  setTextDrag(line)
+  document.body.style.cursor = 'grab'
+}
+
+// function resizeCanvas() {
+//   const elContainer = document.querySelector('.canvas-container')
+
+//   gElCanvas.width = elContainer.offsetWidth
+//   gElCanvas.height = elContainer.offsetHeight
+// }
+
+function getEvPos(ev) {
+  if (TOUCH_EVENTS.includes(ev.type)) {
+    ev.preventDefault() // Prevent triggering the mouse events
+    ev = ev.changedTouches[0] // Gets the first touch point
+
+    // Calculate the touch position inside the canvas
+
+    // ev.pageX = distance of touch position from the documents left edge
+    // target.offsetLeft = offset of the elemnt's left side from the it's parent
+    // target.clientLeft = width of the elemnt's left border
+
+    return {
+      x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+      y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+    }
+  } else {
+    return {
+      x: ev.offsetX,
+      y: ev.offsetY,
+    }
+  }
+}
+
+function txtClicked() {
+  const line = gMeme.lines.find((line) => {
+    var { x, y, fontSize, length = line.txt.length * line.fontSize } = line
+
+    return (
+      gMousePos.x >= x &&
+      gMousePos.x <= length &&
+      gMousePos.y <= y &&
+      gMousePos.y >= y - fontSize
+    )
+  })
+
+  if (!line) return null
+  else return line
+}
+
+function setTextDrag(line) {
+  line.isDrag = !line.isDrag
+}
+
+function moveLine(line, dx, dy) {
+  line.x += dx
+  line.y += dy
+  saveMeme(CURR_MEME_KEY, gMeme)
 }
